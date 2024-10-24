04fb8a2b19479db1947317e863d1da843f38911a
bsuggsfgcu <bcsuggs3857@eagle.fgcu.edu>
2024-10-24 10:10:33 -0400

    Input validation created, still need to test

--- Cimarusti_Paul_Suggs_Raven_Fandino_Cesar_HW3.cpp
+++ Cimarusti_Paul_Suggs_Raven_Fandino_Cesar_HW3.cpp
@@ -1,5 +1,57 @@
 #include <iostream>
 
+//Validate user input for floats
+float GetFloat()
+{
+    //Loop forever, until the function returns valid input
+    while(true)
+    {
+        //String to hold user input
+        std::string input;
+        std::cin >> input;
+        int decimal_counter = 0;
+        bool invalidInLoop = false;
+        if(input.empty())
+        {
+            //Restarts loop to take new input if user somehow entered an empty string
+            std::cout << "Invalid input, try again." << std::endl;
+            continue;
+        }
+        for(int i = 0; i < input.length(); i++)
+        {
+            //If character at index i is not a number then this runs
+            if(!std::isdigit(input[i]))
+            {
+                if(input[i] == '.')
+                {
+                    decimal_counter++;
+                    if(decimal_counter > 1)
+                    {
+                        //If there are too many decimal places, then the input is marked invalid
+                        invalidInLoop = true;
+                        break;
+                    }
+                }
+                else
+                {
+                    invalidInLoop = true;
+                    break;
+                }
+            }
+        }
+        if(invalidInLoop)
+        {
+            //Since we cannot use continue in the for loop above, using bool to delay it until we leave the loop
+            std::cout << "Invalid input, try again." << std::endl;
+            continue;
+        }
+
+        //Since function did not restart at this point, input is valid and returned
+        return atof(input.c_str());
+    }
+}
+
+
 int main()
 {
     std::cout << "Hello, World!" << std::endl;
